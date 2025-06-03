9d94274903a53b36cb31c437161e959b2e36f620
Allyson <jandreyallyson@gmail.com>
2025-06-02 21:31:43 -0300

    Renomear executar_todos para principal. Remoção do modelo.R

--- scripts/executar_todos.R
+++
@@ -1,9 +0,0 @@
-# Carregar os scripts
-source("00_setup.R")
-source("01_carregar_preparar_dados.R")
-source("02_treinar_modelo_e_prever.R")
-source("03_avaliar_modelo.R")
-source("04_gerar_tabela_coeficientes.R")
-
-# Mensagem de confirmação
-print("Todos os scripts foram carregados com sucesso!")
---
+++ scripts/principal.R
@@ -0,0 +1,6 @@
+# Rode esse script para que o script 00 ao 04 sejam rodados de uma só vez
+source("00_setup.R")
+source("01_carregar_preparar_dados.R")
+source("02_treinar_modelo_e_prever.R")
+source("03_avaliar_modelo.R")
+source("04_gerar_tabela_coeficientes.R")
